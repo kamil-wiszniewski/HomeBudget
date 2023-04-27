cc33d760b755b45176273a183710df34f96c1dde
Kamil Wiszniewski <kamil.wiszniewski@whitehill.eu>
2023-04-27 12:45:40 +0200

    Add project files.

---
+++ HomeBudget/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//a Stworzenie(dodanie) nowej transakcji
+//  a1 do wyboru typ
+//  a2 do wyboru kategoria
+//  a3 data, kwota, opis
+
+using HomeBudget;
+
+MenuActionService actionService = new MenuActionService();
+actionService = Initialize(actionService);
+
+var mainMenu = actionService.GetMenuActionsByMenuName("Main");
+
+for (int i = 0; i < mainMenu.Count; i++) 
+{
+    Console.WriteLine($"{mainMenu[i].Id}. {mainMenu[i].Name}");
+}
+
+var operation = Console.ReadKey();
+
+static MenuActionService Initialize(MenuActionService actionService)
+{
+    actionService.AddNewAction(1, "Add new transaction", "Main");
+    actionService.AddNewAction(2, "Delete transaction", "Main");
+    actionService.AddNewAction(3, "View transaction", "Main");
+    actionService.AddNewAction(4, "List of transactions", "Main");
+    return actionService;
+    
+}
